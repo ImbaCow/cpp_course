245daa84c129b09cafa0d6b1c9d545a591c73a9b
Ямбаков Егор <33221394+ImbaCow@users.noreply.github.com>
2017-11-19 11:26:58 +0300

    Add files via upload

---
+++ workshop.3/workshop3.3/main.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <SFML/Graphics.hpp>
+
+void pollEvents(sf::RenderWindow &window, sf::Vertex *pointer, size_t count);
+
+float vectorLenght(sf::Vector2f &argument);
+
+void init(sf::Vertex *pointer, size_t count);
+
+void redrawFrame(sf::RenderWindow &window, sf::Vertex *pointer, size_t count);
+
+void onMouseMove(sf::Vector2f centerPosition, sf::Vector2f mousePosition);
