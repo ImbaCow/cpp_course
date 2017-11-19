7cd1891ac708fc06a5f017effbf9d1bc7e3394b0
Ямбаков Егор <33221394+ImbaCow@users.noreply.github.com>
2017-11-19 11:36:07 +0300

    Add files via upload

--- workshop.3/workshop3.3/main.hpp
+++ workshop.3/workshop3.3/main.hpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include <cmath>
-#include <SFML/Graphics.hpp>
+#include "main.h"
 
-void pollEvents(sf::RenderWindow &window, sf::Vertex *pointer, size_t count);
+void pollEvents(sf::RenderWindow &window, sf::Vertex *pointer, size_t count)
+{
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        switch (event.type)
+        {
+        case sf::Event::Closed:
+            window.close();
+            break;
+        case sf::Event::MouseMoved:
+            pointer[1].position = sf::Vector2f(event.mouseMove.x, event.mouseMove.y);
+            onMouseMove(pointer[0].position, pointer[1].position);
+            break;
+        default:
+            break;
+        }
+    }
+}
 
-float vectorLenght(sf::Vector2f &argument);
+void onMouseMove(sf::Vector2f centerPosition, sf::Vector2f mousePosition)
+{
+    sf::Vector2f delta = mousePosition - centerPosition;
+    std::cout << "distance: " << vectorLenght(delta) << std::endl;
+}
 
-void init(sf::Vertex *pointer, size_t count);
+float vectorLenght(sf::Vector2f &argument)
+{
+    return (pow((pow(argument.x, 2.0) + pow(argument.y, 2.0)), 0.5));
+}
 
-void redrawFrame(sf::RenderWindow &window, sf::Vertex *pointer, size_t count);
+void init(sf::Vertex *pointer, size_t count)
+{
+    pointer[0].position = sf::Vector2f(400, 300);
+    pointer[1].position = sf::Vector2f(400, 300);
+    pointer[0].color = sf::Color::Black;
+    pointer[1].color = sf::Color::Black;
+}
 
-void onMouseMove(sf::Vector2f centerPosition, sf::Vector2f mousePosition);
+void redrawFrame(sf::RenderWindow &window, sf::Vertex *pointer, size_t count)
+{
+    window.clear(sf::Color::White);
+    window.draw(pointer, 2, sf::Lines);
+    window.display();
+}
