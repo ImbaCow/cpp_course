b08311a23d4b929ba0faba085f07345383bab705
Ямбаков Егор <33221394+ImbaCow@users.noreply.github.com>
2017-11-29 16:52:14 +0300

    Add files via upload

--- workshop.3/workshop3.4/Ship.h
+++ workshop.3/workshop3.4/Ship.h
@@ -6,7 +6,10 @@ class Ship : public sf::Drawable
 private:
   void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 
-  void checkWallCollision();
+  void checkWallCollision(); // Чтобы не терять корабль за пределами экрана, перемещаем корабль
+                             // при прохождении через край окна на противоположный конец
+
+  void updatePosition();
 
   sf::Vertex ship[5];
   sf::Vertex shipBoost[3];
@@ -14,16 +17,16 @@ private:
   float rotation = 0;
   sf::Vector2f speed = sf::Vector2f(0, 0);
   sf::Vector2f position = START_POSITION;
-  sf::Vector2f direction = sf::Vector2f(0, 0);
-  sf::Vector2f boostDirection;
+  sf::Vector2f direction = sf::Vector2f(0, 0); // Координата х принимает одно из 3-х значений(-1; 0; 1). -1: Разворот влево. 1: Разворот вправо. 0: Не разворачивается
+                                               // Кордината у принимет одно из 2-х значений(1, 0). 1: Увеличение скорости. 0: Скорость не изменяется.
+
+  sf::Vector2f boostDirection; // Направление вектора ускорения
   float rotationSum = 0;
 
-  sf::Transform transform;
+  sf::Transform transform; // Для разворота системы координат вершин коробля
 
 public:
   void init();
-
-  void updatePosition();
 
   void update(float dt);
 
